ac979c8f212249950a11f87f85aa506809110886
nileshpratap <nileshpratap03@gmail.com>
2022-10-23 21:51:12 +0530

    added Users comp. which takes Useritem comp. as its piece and all the comp. are class based comp.

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,11 +1,25 @@
 import React, { Component } from "react";
+import propTypes from "prop-types";
 
 export class Navbar extends Component {
+  static defaultProps = {
+    title: "Github Finder",
+    icons: "fab fa-github",
+  };
+
+  static propTypes = {
+    title: propTypes.string.isRequired,
+    icons: propTypes.string.isRequired,
+  };
+
   render() {
     return (
-      <div>
-        <h1>hi, there</h1>
-      </div>
+      <nav className="navbar bg-primary">
+        <h1>
+          <i className={this.props.icons}></i>
+          {this.props.title}
+        </h1>
+      </nav>
     );
   }
 }
